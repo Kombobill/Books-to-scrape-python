8406fd9e484f6d175a62946334804f7703d1a65c
Kombobill <billmadru25@gmail.com>
2024-10-16 23:41:06 +0300

    gettin no of books

--- book.py
+++ book.py
@@ -1,8 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Step 1: Fetch the website content
 url = "https://books.toscrape.com/"
 res = requests.get(url)
 htmlData = res.content
+
+# Step 2: Parse the HTML content with BeautifulSoup
 parsedData = BeautifulSoup(htmlData, "html.parser")
-print(parsedData.prettify())
+
+# Step 3: Find all <article> tags with the class 'product_pod'
+books = parsedData.find_all('article', class_='product_pod')
+
+# Step 4: Get the number of books
+num_of_books = len(books)
+print(f"Number of books: {num_of_books}")
+
