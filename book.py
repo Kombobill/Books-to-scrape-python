89a5aaf2fc0b1f3a4f8898510c968f07972b2df2
Kombobill <billmadru25@gmail.com>
2024-11-04 23:10:06 +0300

    findin all elements of class under classic books

--- book.py
+++ book.py
@@ -2,17 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 
 # Step 1: Fetch the website content
-url = "https://books.toscrape.com/"
+url = "https://books.toscrape.com/catalogue/category/books/classics_6/index.html"
 res = requests.get(url)
 htmlData = res.content
 
 # Step 2: Parse the HTML content with BeautifulSoup
 parsedData = BeautifulSoup(htmlData, "html.parser")
 
-# Step 3: Find all <article> tags with the class 'product_pod'
-books = parsedData.find_all('article', class_='product_pod')
+# # Step 3: Find all <article> tags with the class 'product_pod'
+# books = parsedData.find_all('article', class_='product_pod')
 
-# Step 4: Get the number of books
-num_of_books = len(books)
-print(f"Number of books: {num_of_books}")
+# # Step 4: Get the number of books
+# num_of_books = len(books)
+# print(f"Number of books: {num_of_books}")
 
+s = parsedData.find('div', class_='col-sm-8 col-md-9')
+
+
+print(s)
